99c4ac1177c618d991beb60cbc5a8f481e9a9c8d
nickyoung-hashicorp <nyoung@hashicorp.com>
2019-11-22 14:40:21 -0800

    first commit

--- main.tf
+++ main.tf
@@ -13,28 +13,29 @@ variable "windows_dns_prefix" {
 
 variable "admin_password" {
   description = "admin password for Windows VM"
-  default = ""
+  default = "pTFE1234!"
 }
 
 module "windowsserver" {
   source              = "Azure/compute/azurerm"
-    location            = var.location
-  resource_group_name = var.windows_dns_prefix
-  vm_hostname         = "nyoung-azure-tfc-demo"
-  admin_password      = var.admin_password
+  version             = "1.1.5"
+  location            = "${var.location}"
+  resource_group_name = "${var.windows_dns_prefix}-rc"
+  vm_hostname         = "pwc-ptfe"
+  admin_password      = "${var.admin_password}"
   vm_os_simple        = "WindowsServer"
-  public_ip_dns       = var.windows_dns_prefix
-  vnet_subnet_id      = module.network.vnet_subnets[0]
+  public_ip_dns       = ["${var.windows_dns_prefix}"]
+  vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
 }
 
 module "network" {
   source              = "Azure/network/azurerm"
-
-  location            = var.location
-  resource_group_name = var.windows_dns_prefix
+  version             = "1.1.1"
+  location            = "${var.location}"
+  resource_group_name = "${var.windows_dns_prefix}-rc"
   allow_ssh_traffic   = true
 }
 
-output "windows_vm_public_name" {
-  value = module.windowsserver.public_ip_dns_name
+output "windows_vm_public_name"{
+  value = "${module.windowsserver.public_ip_dns_name}"
 }
