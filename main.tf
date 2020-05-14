1cb98afe47737e24496ff77b20b02e51abd70040
Nick Young <57373279+nickyoung-hashicorp@users.noreply.github.com>
2020-05-14 07:25:00 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,29 +13,28 @@ variable "windows_dns_prefix" {
 
 variable "admin_password" {
   description = "admin password for Windows VM"
-  default = "pTFE1234!"
 }
 
 module "windowsserver" {
   source              = "Azure/compute/azurerm"
   version             = "1.1.5"
   location            = "${var.location}"
-  resource_group_name = "${var.windows_dns_prefix}-rc"
-  vm_hostname         = "pwc-ptfe"
+  resource_group_name = "${var.windows_dns_prefix}-rg"
+  vm_hostname         = "{var.windows_dns_prefix}-azure-vm"
   admin_password      = "${var.admin_password}"
   vm_os_simple        = "WindowsServer"
   public_ip_dns       = ["${var.windows_dns_prefix}"]
   vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
-  tags = {
-    environment = "demo"
-  }
+#   tags = {
+#     environment = "demo"
+#   }
 }
 
 module "network" {
   source              = "Azure/network/azurerm"
   version             = "1.1.1"
   location            = "${var.location}"
-  resource_group_name = "${var.windows_dns_prefix}-rc"
+  resource_group_name = "${var.windows_dns_prefix}-rg"
   allow_ssh_traffic   = true
   tags = {
     environment = "demo"
