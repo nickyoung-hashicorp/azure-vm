0a1c071b0f131671d0bfb1cacb387e0c7e181f99
Nick Young <57373279+nickyoung-hashicorp@users.noreply.github.com>
2021-05-12 10:54:06 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,43 +6,55 @@ terraform {
     }
   }
 }
+    
+# provider "azurerm" {
+#   features {}
+# }
 
-variable "location" {
-  description = "Azure location in which to create resources"
-  default = "West US"
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
 }
 
-variable "windows_dns_prefix" {
-  description = "DNS prefix to add to to public IP address for Windows VM"
-}
-
-variable "admin_password" {
-  description = "admin password for Windows VM"
-}
-
-module "windowsserver" {
+module "linuxservers" {
   source              = "Azure/compute/azurerm"
-  version             = "1.1.5"
-  location            = "${var.location}"
-  resource_group_name = "${var.windows_dns_prefix}-rg"
-  vm_hostname         = "azure-vm"
-  admin_password      = "${var.admin_password}"
+  resource_group_name = azurerm_resource_group.example.name
+  vm_os_simple        = "UbuntuServer"
+  public_ip_dns       = ["linsimplevmips"] // change to a unique name per datacenter region
+  vnet_subnet_id      = module.network.vnet_subnets[0]
+
+  depends_on = [azurerm_resource_group.example]
+}
+
+module "windowsservers" {
+  source              = "Azure/compute/azurerm"
+  resource_group_name = azurerm_resource_group.example.name
+  is_windows_image    = true
+  vm_hostname         = "mywinvm" // line can be removed if only one VM module per resource group
+  admin_password      = "ComplxP@ssw0rd!"
   vm_os_simple        = "WindowsServer"
-  public_ip_dns       = ["${var.windows_dns_prefix}"]
-  vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
-  tags = {
-    environment = "devops"
-  }
+  public_ip_dns       = ["winsimplevmips"] // change to a unique name per datacenter region
+  vnet_subnet_id      = module.network.vnet_subnets[0]
+
+  depends_on = [azurerm_resource_group.example]
+#   tags = {
+#     environment = "devops"
+#   }
 }
 
 module "network" {
   source              = "Azure/network/azurerm"
-  version             = "1.1.1"
-  location            = "${var.location}"
-  resource_group_name = "${var.windows_dns_prefix}-rg"
-  allow_ssh_traffic   = true
+  resource_group_name = azurerm_resource_group.example.name
+  subnet_prefixes     = ["10.0.1.0/24"]
+  subnet_names        = ["subnet1"]
+
+  depends_on = [azurerm_resource_group.example]
 }
 
-output "windows_vm_public_name"{
-  value = "${module.windowsserver.public_ip_dns_name}"
+output "linux_vm_public_name" {
+  value = module.linuxservers.public_ip_dns_name
+}
+
+output "windows_vm_public_name" {
+  value = module.windowsservers.public_ip_dns_name
 }
