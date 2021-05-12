145f8148f0e5ad900fe21d0883abc07331673838
Nick Young <57373279+nickyoung-hashicorp@users.noreply.github.com>
2021-05-12 10:48:57 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 terraform {
-  required_version = ">= 0.11.1"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.46.0"
+    }
+  }
 }
 
 variable "location" {
